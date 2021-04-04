b83781f1a0ca6a1bbb263c6f8748f8d8d5301986
RHAF <RHAF05@hotmail.com>
2021-04-03 19:39:25 -0500

    ajustes varios

--- select.py
+++ select.py
@@ -9,7 +9,15 @@ now = datetime.datetime.now()
 fecha_ant = str(now.year) + '-0' + str(now.month) + '-0' + str(now.day)
 
 
-print("SELECT * FROM poles WHERE pole='pole' AND date >= '" +str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " 00:00:00 0.000'")
-# datos = cursor.execute("SELECT * FROM personas where nombre='' AND id>0")
-datos = cursor.execute("SELECT * FROM poles WHERE pole='pole' AND date >= '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + " 00:00:00 0.000'")
-print(len(datos.fetchall()))
+# print("SELECT * FROM poles WHERE pole='pole' AND date >= '" +str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " 00:00:00 0.000'")
+datos = cursor.execute("SELECT SUM(points) AS puntos, user FROM poles GROUP BY user")
+# datos = cursor.execute("SELECT * FROM poles WHERE pole='pole' AND date >= '" +str(now.year) + "-" + str(now.month).zfill(2) + "-" + str(now.day).zfill(2) + " 00:00:00 0.000'")
+
+poles = datos.fetchall()
+
+registros = "Estos son los registros\n"
+
+for pole in poles:
+    registros += str(pole[1]) + ": " + str(pole[0]) + " puntos\n"
+
+print(registros)
